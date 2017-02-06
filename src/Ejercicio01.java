6eb5c97b50eabc65f6b0888316dc55ab67499ad0
Raúl Martín Garcés <1raulmartin1@gmail.com>
2017-02-06 22:51:39 +0100

    Add files via upload

---
+++ src/Ejercicio01.java
@@ -0,0 +1,20 @@
+
+public class Ejercicio01 {
+	public static void main(String[] args){
+		
+		PilotoDeFormula1 g1 = new PilotoDeFormula1();
+		PilotoDeFormula1 g2 = new PilotoDeFormula1();
+		
+		g1.nombre = "Fernando Alonso";
+		g1.equipo = "Ferrari";
+		g1.NPremios = 1;
+		
+		g2.nombre = "Better";
+		g2.equipo = "Red Bull";
+		
+		System.out.print(g2.nombre);
+		System.out.print(g1.NPremios);
+
+	}
+	
+}
